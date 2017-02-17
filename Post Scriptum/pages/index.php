362ca36c3b36fa2abedcb38e412682721bdfd8a1
Haris <himamovic1@etf.unsa.ba>
2017-02-17 23:56:12 +0100

    Full Update

---
+++ Post Scriptum/pages/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html lang="bs">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Post Scriptum novinski portal pocetna">
+	<meta name="keywords" content="sarajevo,vijesti,novine,portal">
+
+	<title>Post Scriptum - Pocetna</title>
+
+	<link rel="stylesheet" href="../style/style.css">
+</head>
+
+<body>
+
+	<!-- Header -->
+	<header>
+		<!-- Banner -->
+		<div id="banner"><h2 class="uppercase title title-white">Post scriptum</h2></div>
+
+		<!-- Navigation bar -->
+		<nav id="navBar">	
+			<ul>
+				<li>
+					<img id="menuToggleButton" src="../icons/menu.svg" alt="Menu" onclick="toggleNavBar()">
+					<a href="index.php" id="logo" class="uppercase">Post Scriptum</a>
+				</li>
+				<li class="navBarContent">
+					<a href="index.php" class="active"> Početna </a>
+				</li>
+				<li class="navBarContent">
+					<a href="top_price.php"> Top priče </a>
+				</li>
+				<li class="navBarContent">
+					<a href="svijet_regija.php"> Svijet/Regija </a>
+				</li>
+				<li class="navBarContent">
+					<a href="biznis.php"> Biznis </a>
+				</li>
+				<li class="navBarContent">
+					<a href="sport.php"> Sport </a>
+				</li>
+				<li class="navBarContent">
+					<a href="kultura.php"> Kultura </a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+	<!-- Main content container -->
+	<div id="mainContent">
+		<div class="row">
+			<div class="cspan-6"><h5 class="title">Hello World!</h5></div>
+		</div>
+		
+	</div>
+
+	<script src="../scripts/javascript.js"></script>
+</body>
+
+</html>
---
+++ Post Scriptum/scripts/javascript.js
@@ -0,0 +1,7 @@
+// Function toggles navigation menu
+function toggleNavBar() {
+    var navBarItems = document.getElementsByClassName("navBarContent");
+
+	for (var i = 0; i < navBarItems.length; i++) 
+		navBarItems[i].classList.toggle("show");
+}
