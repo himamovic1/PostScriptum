d622bf171f8efc7092701bf6025322521d8516ba
Almir Hamza <ahamza1@etf.unsa.ba>
2017-02-18 17:31:56 +0100

    Dodan sadrzaj
    Dodan footer i reset kod, te popravljen banner i header.

--- Post Scriptum/pages/index.php
+++ Post Scriptum/pages/index.php
@@ -16,45 +16,138 @@
 
 	<!-- Header -->
 	<header>
+		<!-- Top bar 
+		<ul id="topBar" class="noDisp">
+			<li>
+				<a href="" class="title-white">Kontakt informacije</a>
+			</li>
+			<li>
+				<a href="" class="title-white">Marketing | info@example.com</a> 
+			</li>
+			<li>
+				<a href="" class="title-white">Pratite nas na facebook-u</a>
+			</li>
+		</ul>
+		-->
+
 		<!-- Banner -->
-		<div id="banner"><h2 class="uppercase title title-white">Post scriptum</h2></div>
+		<div id="banner" class="row">
+			<h1 id="topBarLogo" class="uppercase page-title">Post scriptum</h1>
+		</div>
 
 		<!-- Navigation bar -->
-		<nav id="navBar">	
+		<nav id="navBar">
+			<div class="container">
+				<ul>
+					<li>
+						<img id="menuToggleButton" src="../icons/menu.svg" alt="Menu" onclick="toggleNavBar()">
+						<a href="index.php" class="logo uppercase">Post Scriptum</a>
+					</li>
+					<li class="navBarContent">
+						<a href="index.php" class="active"> Početna </a>
+					</li>
+					<li class="navBarContent">
+						<a href="top_price.php"> Top priče </a>
+					</li>
+					<li class="navBarContent">
+						<a href="svijet_regija.php"> Svijet/Regija </a>
+					</li>
+					<li class="navBarContent">
+						<a href="biznis.php"> Biznis </a>
+					</li>
+					<li class="navBarContent">
+						<a href="sport.php"> Sport </a>
+					</li>
+					<li class="navBarContent">
+						<a href="kultura.php"> Kultura </a>
+					</li>
+				</ul>
+			</div>	
+		</nav>
+	</header>
+	
+	<!-- Main Content Container -->
+	<div class="container">
+
+		<!-- Top Story Section -->
+
+		<!-- News Section -->
+		<section>
+			<div>Naslov</div>
+
+			<!-- Large article -->
+			<article class="cspan-4">
+				<a href="">
+					<div class="image-container-big">
+						<img src="../img/img1.png" alt="Article image" >
+					</div>
+
+					<span class="world_region category-outlined"><small>BiH</small></span>
+					<h2 class="title">Organizatori Diplomatskog bazara uručili 130.000 KM za pomoć djeci u BiH</h2>
+					<p>Organizacioni odbor Diplomatskog bazara 2016 danas je dodijelio 130.000 KM dobrotvornim organizacijama za djecu s potrebama iz Bosne i Hercegovine i to je do sada najveći prikupljeni iznos.</p>		
+				</a>
+			</article>
+
+			<!-- More news -->
+			<div class="cspan-5">
+				<a href="" class="row">
+					<div class="image-container-sml cspan-3">
+						<img src="../img/img1.png" alt="Article image" >
+					</div>
+					<div class="cspan-9">
+						<span class="world_region category-outlined"><small>BiH</small></span>
+						<h5 class="title">Organizatori Diplomatskog bazara uručili 130.000 KM za pomoć djeci u BiH</h5>
+					</div>
+				</a>
+
+				<a href="" class="row">
+					<div class="image-container-sml cspan-3">
+						<img src="../img/img1.png" alt="Article image" >
+					</div>
+					<div class="cspan-9">
+						<span class="world_region category-outlined"><small>BiH</small></span>
+						<h5 class="title">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</h5>
+					</div>
+				</a>
+			</div>
+
+			<!-- Most read articles -->
+			<div class="cspan-3">
+				<a href="" class="row">
+					<div class="cspan-12">
+						<span class="world_region category-outlined"><small>BiH</small></span>
+						<h5 class="title">Koridor 5C kroz Hercegovinu: Ratnom linijom razgraničenja i protiv interesa stanovništva</h5>
+					</div>
+				</a>
+			</div>
+		</section>
+
+		<!-- Sport Section -->
+
+		<!-- Lifestyle section -->
+
+	</div>
+
+	<!-- Footer -->
+	<footer id="pageFooter">
+		<nav class="container">
 			<ul>
 				<li>
-					<img id="menuToggleButton" src="../icons/menu.svg" alt="Menu" onclick="toggleNavBar()">
-					<a href="index.php" id="logo" class="uppercase">Post Scriptum</a>
-				</li>
-				<li class="navBarContent">
-					<a href="index.php" class="active"> Početna </a>
-				</li>
-				<li class="navBarContent">
-					<a href="top_price.php"> Top priče </a>
-				</li>
-				<li class="navBarContent">
-					<a href="svijet_regija.php"> Svijet/Regija </a>
-				</li>
-				<li class="navBarContent">
-					<a href="biznis.php"> Biznis </a>
-				</li>
-				<li class="navBarContent">
-					<a href="sport.php"> Sport </a>
-				</li>
-				<li class="navBarContent">
-					<a href="kultura.php"> Kultura </a>
+					<a href="index.php" class="logo uppercase">Post Scriptum</a>
+				</li><li class="navBarContent">
+					<a href="index.php" class="active"> O nama </a>
+				</li><li class="navBarContent">
+					<a href="top_price.php"> Impressum </a>
+				</li><li class="navBarContent">
+					<a href="svijet_regija.php"> Marketing </a>
+				</li><li class="navBarContent">
+					<a href="biznis.php"> Dojavi vijest </a>
+				</li><li class="navBarContent">
+					<a href="sport.php"> Uslovi korištenja </a>
 				</li>
 			</ul>
 		</nav>
-	</header>
-
-	<!-- Main content container -->
-	<div id="mainContent">
-		<div class="row">
-			<div class="cspan-6"><h5 class="title">Hello World!</h5></div>
-		</div>
-		
-	</div>
+	</footer>
 
 	<script src="../scripts/javascript.js"></script>
 </body>
