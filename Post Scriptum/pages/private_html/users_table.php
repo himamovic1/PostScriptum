017c6499affbd072542a4ce08984a76b359731a1
Haris Imamović <himamovic1@etf.unsa.ba>
2017-02-21 16:45:01 +0100

    Added users table

---
+++ Post Scriptum/pages/private_html/users_table.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="bs">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=0.9, maximum-scale=1, user-scalable=no">
+	<meta name="description" content="Post Scriptum novinski portal pocetna">
+	<meta name="keywords" content="sarajevo,vijesti,novine,portal">
+
+	<title>Post Scriptum - Pocetna</title>
+
+	<link rel="stylesheet" href="../../css/bootstrap.css">
+	<link rel="stylesheet" href="../../css/style_admin_panel.css">
+</head>
+
+<body>
+
+	<div class="container">
+		<div class="row">
+			<h1>Pregled svih prijavljenih korisnika</h1>
+		</div>
+
+		<div class="row">
+			<table id="users-table" class="table table-hover">
+				<thead>
+					<tr>
+						<th>Avatar</th>
+						<th>Ime</th>
+						<th>Prezime</th>
+						<th>Korisničko ime</th>
+						<th>Email</th>
+						<th class="text-center">Dodatne opcije</th>
+					</tr>
+				</thead>
+
+				<tbody>
+		            <tr>
+		                <td><img class="img-circle usr-avatar-sm" src="../../img/img1.png"></td>
+		                <td>John</td>
+		                <td>Carter</td>
+		                <td>thelegend27</td>
+		                <td>johncarter@mail.com</td>
+		                <td><button class="btn btn-danger center-block">Banuj korisnika</button></td>
+		            </tr>
+		            <tr>
+		                <td><img class="img-circle usr-avatar-sm" src="../../icons/user.svg"></td>
+		                <td>Peter</td>
+		                <td>Parker</td>
+		                <td>thelegend27</td>
+		                <td>peterparker@mail.com</td>
+		                <td><button class="btn btn-danger center-block">Banuj korisnika</button></td>
+		            </tr>
+		            <tr>
+		                <td><img class="img-circle usr-avatar-sm" src="../../icons/user.svg"></td>
+		                <td>John</td>
+		                <td>Rambo</td>
+		                <td>thelegend27</td>
+		                <td>johnrambo@mail.com</td>
+		                <td><button class="btn btn-danger center-block">Banuj korisnika</button></td>
+		            </tr>
+		        </tbody>
+			</table>
+		</div>
+	</div>
+
+
+</body>
+
+</html>
