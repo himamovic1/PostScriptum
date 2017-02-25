91ccbc5446862782b5c306ef51486d941bad9360
Almir Hamza <ahamza1@etf.unsa.ba>
2017-02-25 19:39:38 +0100

    Zatvoreno.

--- Post Scriptum/php/odjava.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	session_start();
-
-	if(isset($_SESSION['username']) && isset($_SESSION['role'])) {
-		session_unset();
-		header("Location: ../pages/index.php"); // Redirect to home page
-	}
- ?>
--- Post Scriptum/php/tmp.php
+++
@@ -1,1 +0,0 @@
-<?php echo password_hash('jessdoe', PASSWORD_DEFAULT); ?>
